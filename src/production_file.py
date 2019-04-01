d8226da5ca9de1e8e950ff63e090e91173b67371
Isaack Mwenda <mwendaisaac@gmail.com>
2019-04-01 14:15:52 +0300

    Separated Follow Up coding plans & RQA coding plan

--- src/production_file.py
+++ src/production_file.py
@@ -6,14 +6,16 @@ class ProductionFile(object):
     @staticmethod
     def generate(data, production_csv_output_path):
         production_keys = ["uid"]
-        for plan in PipelineConfiguration.RQA_AND_FOLLOW_UP_CODING_PLANS:
+        for plan in PipelineConfiguration.RQA_CODING_PLANS:
+            if plan.raw_field not in production_keys:
+                production_keys.append(plan.raw_field)
+        for plan in PipelineConfiguration.FOLLOW_UP_CODING_PLANS:
             if plan.raw_field not in production_keys:
                 production_keys.append(plan.raw_field)
         for plan in PipelineConfiguration.DEMOGS_CODING_PLANS:
             if plan.raw_field not in production_keys:
                 production_keys.append(plan.raw_field)
                 
-        # Not perfoming message filtering at this stage for this test-pipeline.
         with open(production_csv_output_path, "w") as f:
             TracedDataCSVIO.export_traced_data_iterable_to_csv(data, f, headers=production_keys)
         
