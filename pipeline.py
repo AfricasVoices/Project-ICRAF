732ba7ff606cd3c20b2948bd8d8df86e493a090c
Isaack Mwenda <mwendaisaac@gmail.com>
2019-07-17 15:20:07 +0300

    import FilterNOP

--- pipeline.py
+++ pipeline.py
@@ -9,7 +9,8 @@ from google.cloud import storage
 from storage.google_drive import drive_client_wrapper
 
 from src import CombineRawDatasets, TranslateRapidProKeys, AutoCodeShowMessages, \
-    ProductionFile, AutoCodeSurveys, ApplyManualCodes, AnalysisFile, AdvertPhoneNumbers, WSCorrection
+    ProductionFile, AutoCodeSurveys, ApplyManualCodes, AnalysisFile, AdvertPhoneNumbers, WSCorrection, \
+    FilterNOP
 
 from src.lib import PipelineConfiguration
 
@@ -181,7 +182,7 @@ if __name__ == "__main__":
 
      print("Generating Analysis CSVs...")
      data = AnalysisFile.generate(user, data, csv_by_message_output_path, csv_by_individual_output_path)
-     
+
      print("Writing TracedData to file...")
      IOUtils.ensure_dirs_exist_for_file(json_output_path)
      with open(json_output_path, "w") as f:
